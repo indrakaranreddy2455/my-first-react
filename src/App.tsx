580bce66c20588607e6dc639aa29cefdd7566449
indrakaranreddy <you@example.com>
2024-12-21 14:07:34 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Products from './components/Products'
+import Photos from './vehicle-components/Photo'
+
+export default function products() {
+  return (
+    <div>
+    <Photos/>
+    </div>
+  )
+}
